5d30380fb8ba3e795280ab9fe9d6106317ec430a
Jason Sinn <jrs341@me.com>
2017-04-09 12:09:03 -0500

    added babel-loader to webpack config instead of just babel

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// var path = require("path");
+
+module.exports = {
+  entry: "./app/app.js",
+  output: {
+    // path: path.resolve(__dirname, "build"),
+    // publicPath: "/public/",
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        include: /app/,
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015", "stage-0"],
+          plugins: ["transform-decorators-legacy"],
+        }
+      }
+    ]
+  }
+  // This lets us debug our react code in chrome dev tools. Errors will have lines and file names
+  // Without this the console says all errors are coming from just coming from bundle.js
+  // devtool: "eval-source-map",
+  // devServer: {
+  //   historyApiFallback: true,
+  //   contentBase: './',
+  //   hot: true
+  // }
+};
