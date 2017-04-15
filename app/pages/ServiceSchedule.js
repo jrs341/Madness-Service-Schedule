d3e9e3df0c43b58b7ed8b37cc3a63b1ec7762202
Jason Sinn <jrs341@me.com>
2017-04-15 11:18:13 -0500

    info is mapping to table in service schedule

--- app/pages/ServiceSchedule.js
+++ app/pages/ServiceSchedule.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from "react-redux"
 import { Link } from 'react-router'
-import { Row } from 'react-grid-system'
+import { Row, Col } from 'react-grid-system'
+import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card'
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table'
 import axios from 'axios'
 import LocationDropDown from '../Components/LocationDropDown'
 import DatePicker from '../Components/DatePicker'
@@ -31,6 +33,7 @@ export default class ServiceSchedule extends React.Component {
 constructor(props) {
 	super(props);
 	this.state = {
+		scheduleInfo: [],
 		given_name: '',
 		family_name: '',
 		phone_number: '',
@@ -43,13 +46,34 @@ constructor(props) {
 	};
 
 	this.scheduleForToday = this.scheduleForToday.bind(this);
-
+	this.tableRow = this.tableRow.bind(this);
+	this.updateScheduleInfo = this.updateScheduleInfo.bind(this);
 }
 
 componentWillMount() {
 	// get request here for current date
 	console.log(dateString);
 	this.scheduleForToday();
+}
+
+tableRow(fieldInfo) {
+    return (
+      <TableRow>
+      	<TableRowColumn>{fieldInfo.time}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.given_name}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.family_name}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.email}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.phone_number}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.vehicle_year}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.vehicle_make}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.vehicle_model}</TableRowColumn>
+        <TableRowColumn>{fieldInfo.requested_service}</TableRowColumn>
+      </TableRow>
+    );
+}
+
+updateScheduleInfo(getRequestResponse) {
+    this.setState({scheduleInfo: getRequestResponse});
 }
 
 scheduleForToday = () => {
@@ -64,10 +88,16 @@ scheduleForToday = () => {
     	}
     	else {
     		console.log('today s schedule');
-    		for (var i=0; i<response.data.length; i++) {
-    			this.state = response.data[i];
-    			console.log(response.data[i]);
-    		}
+    		console.log(response.data.length);
+    		this.updateScheduleInfo(response.data);
+    		console.log(this.state.scheduleInfo);
+    		// this.updateScheduleInfo(response.data);
+    		// for (var i=0; i<response.data.length; i++) {
+    		// 	console.log(i);
+    		// 	this.updateScheduleInfo(response.data[i]);
+    		// 	// this.state = response.data[i];
+    		// 	console.log(response.data[i]);
+    		// }
     		// this.state = response.data;
     		// for (var prop in response.data) {
     		// 	// console.log(prop + ' = ' + response.data[prop]);
@@ -99,7 +129,46 @@ scheduleForToday = () => {
     		<h1> Madness AutoWorks Service Schedule for <LocationDropDown /> <DatePicker />  </h1>
     		 {/*<h1> {this.state.given_name} </h1>
     		 <h1> {this.state.family_name} </h1>*/}
-    	</div>
+    	<Row>
+        <Col md={12}>
+          <Card>
+            <CardTitle
+              title="Madness AutoWorks Service Schedule"
+              subtitle={this.state.instructions}
+            />
+            <CardText>
+            <Table>
+              <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                <TableRow>
+                  <TableHeaderColumn>Time</TableHeaderColumn>
+                  <TableHeaderColumn>First Name</TableHeaderColumn>
+                  <TableHeaderColumn>Last Name</TableHeaderColumn>
+                  <TableHeaderColumn>Email</TableHeaderColumn>
+                  <TableHeaderColumn>Phone</TableHeaderColumn>
+                  <TableHeaderColumn>Year</TableHeaderColumn>
+                  <TableHeaderColumn>Make</TableHeaderColumn>
+                  <TableHeaderColumn>Model</TableHeaderColumn>
+                  <TableHeaderColumn>Service</TableHeaderColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody displayRowCheckbox={false} stripedRows={true}>
+              {this.state.scheduleInfo.map((fieldInfo, index) => this.tableRow(fieldInfo, index))}
+              </TableBody>
+            </Table>
+            </CardText>
+            <CardActions>
+            {/*<Link to='admin'>
+             <RaisedButton
+              style={submitButton}
+              label="Return to Admin"
+              primary={true}
+              />
+            </Link>*/}
+            </CardActions>
+          </Card>
+        </Col>
+      </Row>
+    </div>
     );
   }
 }
